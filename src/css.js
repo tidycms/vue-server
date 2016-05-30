ecb5e48a8b5cfe39fe16412c66e35a0aad0efbff
Pyry Liukas <pyry@tidycms.com>
2016-05-30 12:11:09 +0300

    if item = " ", then skip item and do not throw error

--- src/css.js
+++ src/css.js
@@ -1,39 +1,43 @@
 var CSSParser = {
-    parse: function (string) {
-        var temp = string.split(';');
-        var obj = {};
+  parse: function (string) {
+    var temp = string.split(';');
+    var obj = {};
+    try {
 
-        for (var i = 0; i < temp.length; i++) {
-            (function () {
-                var item = temp[i];
-                if (item) {
-                    var prop = item.split(/\:(.+)/);
+      for (var i = 0; i < temp.length; i++) {
+        (function () {
+          var item = temp[i];
+          if (item && item.trim().length > 0) {
+            var prop = item.split(/\:(.+)/);
 
-                    if (!prop[1]) {
-                        throw 'CSS format is invalid: "' + item + '"';
-                    }
-
-                    obj[prop[0].trim()] = prop[1].trim();
-                }
-            })();
-        }
-
-        return obj;
-    },
-
-    stringify: function (object) {
-        var string = '';
-
-        for (var prop in object) {
-            if (object[prop] === undefined || object[prop] === null || object[prop] === '') {
-                continue;
+            if (!prop[1]) {
+              throw 'CSS format is invalid: "' + item + '"';
             }
-            string += prop.replace(/[A-Z]/g, function (a) { return '-' + a.toLowerCase(); }) + ': ' + object[prop] + '; ';
-            // string += prop + ': ' + object[prop] + '; ';
-        }
 
-        return string.trim();
+            obj[prop[0].trim()] = prop[1].trim();
+          }
+        })();
+      }
+
+    } catch (err) {
+      console.error('CSS PARSER', err);
     }
+    return obj;
+  },
+
+  stringify: function (object) {
+    var string = '';
+
+    for (var prop in object) {
+      if (object[prop] === undefined || object[prop] === null || object[prop] === '') {
+        continue;
+      }
+      string += prop.replace(/[A-Z]/g, function (a) { return '-' + a.toLowerCase(); }) + ': ' + object[prop] + '; ';
+      // string += prop + ': ' + object[prop] + '; ';
+    }
+
+    return string.trim();
+  }
 };
 
 module.exports = CSSParser;
